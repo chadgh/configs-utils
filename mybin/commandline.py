2dacdf943076b6358803c22ddf37d3d847f57dc6
chadgh <chadgh@gmail.com>
2012-06-14 07:52:03 -0600

    updated scripts

--- mybin/commandline.py
+++ mybin/commandline.py
@@ -9,4 +9,5 @@ def getArgParser(program=__file__, desc=''):
     """Generates a default argparser for command line python scripts."""
     parser = argparse.ArgumentParser(prog=program, description=desc)
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help="display actual commands executed")
+    parser.add_argument('-t', '--test', action='store_true', default=False, help="display actual commands executed, but don't execute")
     return parser
--- mybin/dailyreport.py
+++ mybin/dailyreport.py
@@ -2,7 +2,7 @@
 import sys
 from commandline import local, getArgParser
 
-parser = getArgParser(__file__, 'Sends daily report.')
+parser = getArgParser(__file__.split('/')[-1], 'Sends daily report.')
 parser.add_argument('-d', '--duration', default=10, dest='duration', help="indicate the amount of time to go back.")
 parser.add_argument('-u', '--unit', default='h', dest='unit', help="indicates the unit of measurement to be used [m,h,d,w]")
 
@@ -12,6 +12,8 @@ selfstat = local["/usr/bin/selfstats.py"]
 mail = local["/usr/bin/mail"]
 
 cmd = selfstat["-b", args.duration, args.unit, "--ratios", "--periods", "--active", "--pactive"] | mail['-s', 'Daily Stats', "chadgh@gmail.com"]
-if args.verbose:
+
+if args.verbose or args.test:
     print(cmd)
-cmd()
+if not args.test:
+    cmd()
